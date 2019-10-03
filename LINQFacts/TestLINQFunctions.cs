e880a7a66cde14a83370f70f274d6e9242276613
thisisalin <alintroncea@gmail.com>
2019-10-03 14:42:58 +0300

    implemented ThenBy and ThenBy test

--- LINQFacts/TestLINQFunctions.cs
+++ LINQFacts/TestLINQFunctions.cs
@@ -7,7 +7,27 @@ namespace LINQ
 {
     public class TestLINQFunctions
     {
+        [Fact]
+        public void TestThenBy()
+        {
+            var students = new List<Student>() {
+            new Student(1, "John", "Computer Science"),
+            new Student(2, "Steve", "Biology"),
+            new Student(3, "Bill", "Math"),
+            new Student(4, "Ram", "Biology"),
+            new Student(5, "Ron", "Math"),
+            new Student(6, "Ram", "Computer Science"),
+        };
+            Func<Student, string> nameSelector = x => x.Name;
+            Func<Student, int> idSelector = x => x.ID;
+            Func<Student, string> classSelector = x => x.Class;
 
+            var result = LINQFunctions.OrderBy(students, x => nameSelector(x), new KeyComparer<string>()).
+               ThenBy(y => idSelector(y), new KeyComparer<int>()).
+               ThenBy(z => classSelector(z), new KeyComparer<string>());
+
+            Assert.Equal("Steve", result.Last().Name);
+        }
         [Fact]
         public void TestOrderBy()
         {
